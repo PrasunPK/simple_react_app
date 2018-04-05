47bc444bd4a42ecc8ff2252c138db68a248dd568
PrasunPK <prasun_1993@rediffmail.com>
2018-04-05 20:59:18 +0530

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import logo from '../logo.svg';
+
+export class Header extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div>
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">Virtual Market</h1>
+                    </div>
+                    <div className="greetings">
+                        <name> First name</name>
+                    </div>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
